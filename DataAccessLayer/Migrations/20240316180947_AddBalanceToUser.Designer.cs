306ad536c9960162221fad41a6976dddc1906db0
HighLink1001 <caolinh1012003@gmail.com>
2024-03-17 14:16:25 +0700

    Add balance

--- DataAccessLayer/Migrations/20240316180947_AddBalanceToUser.Designer.cs
+++ DataAccessLayer/Migrations/20240316180947_AddBalanceToUser.Designer.cs
@@ -4,6 +4,7 @@ using DataAccessLayer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace DataAccessLayer.Migrations
 {
     [DbContext(typeof(BookingPartyDataContext))]
-    partial class BookingPartyDataContextModelSnapshot : ModelSnapshot
+    [Migration("20240316180947_AddBalanceToUser")]
+    partial class AddBalanceToUser
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ DataAccessLayer/Migrations/20240316180947_AddBalanceToUser.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DataAccessLayer.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddBalanceToUser : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<decimal>(
+                name: "Balance",
+                table: "Users",
+                type: "decimal(18,2)",
+                nullable: false,
+                defaultValue: 0m);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Balance",
+                table: "Users");
+        }
+    }
+}
